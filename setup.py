75f23779d510a15be9ea78353c8d06d91e708352
gab <gabrielbordeaux@gmail.com>
2017-07-05 22:34:03 -0400

    remove download url

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup (
     author = 'Gabriel Bordeaux',
     author_email = 'pypi@gab.lc',
     url = 'https://github.com/gabfl/vault',
-    download_url = 'https://github.com/gabfl/vault/archive/1.3.tar.gz',
     license = 'MIT',
     packages = ['vault', 'vault.lib'],
     install_requires = ['pycryptodome', 'pyperclip', 'tabulate', 'argparse'], # external dependencies
