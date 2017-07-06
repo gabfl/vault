c500eda53c1ad6dda15ec20000fd7a471e8ffd4a
gab <gabrielbordeaux@gmail.com>
2017-07-05 23:12:26 -0400

    Support for PyPI readme

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup
 
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
 setup (
     name = 'pyvault',
-    version = '1.3',
+    version = '1.3b',
     description = 'Python password manager',
+    long_description = long_description,
     author = 'Gabriel Bordeaux',
     author_email = 'pypi@gab.lc',
     url = 'https://github.com/gabfl/vault',
