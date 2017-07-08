cab6cfcecb106f8c7932be82d3c60c852d234aed
gab <gabrielbordeaux@gmail.com>
2017-07-08 15:44:06 -0400

    remove extra code

--- src/lib/ImportExport.py
+++ src/lib/ImportExport.py
@@ -1,10 +1,6 @@
 # Facilitate relative import
-import os, sys
-path = os.path.dirname(sys.modules[__name__].__file__)
-path = os.path.join(path, '..')
-sys.path.insert(0, path)
 
-import json
+import os, sys, json
 
 """
     Adding import or export formats:
