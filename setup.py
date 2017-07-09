8e9ea19864a9c88ffe0153d33294823dfdc58b52
gab <gabrielbordeaux@gmail.com>
2017-07-08 20:33:46 -0400

    Cosmetic change

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except(IOError, ImportError):
 
 setup (
     name = 'pyvault',
-    version = '1.4.1',
+    version = '1.4.3',
     description = 'Python password manager',
     long_description = long_description,
     author = 'Gabriel Bordeaux',
