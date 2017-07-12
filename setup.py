c45468534c2f6ffa5eae28d2fb4a4cc9438b2317
gab <gabrielbordeaux@gmail.com>
2017-07-12 11:57:33 -0400

    Add manifest

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except(IOError, ImportError):
 
 setup(
     name='pyvault',
-    version='1.4.4.3',
+    version='1.4.4.4',
     description='Python password manager',
     long_description=long_description,
     author='Gabriel Bordeaux',
