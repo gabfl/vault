e6d90a61fdcf78f1ab5b3f13398a66f5c1eb20b1
gab <gabrielbordeaux@gmail.com>
2017-11-06 08:54:55 -0500

    Bump version to fix build

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except(IOError, ImportError):
 
 setup(
     name='pyvault',
-    version='1.8',
+    version='1.8.1',
     description='Python password manager',
     long_description=long_description,
     author='Gabriel Bordeaux',
