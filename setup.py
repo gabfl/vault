cfce29012632c350e68c4c6a85022b966e816305
Gabriel Bordeaux <gabrielbordeaux@gmail.com>
2018-06-18 08:57:28 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except(IOError, ImportError):
 
 setup(
     name='pyvault',
-    version='2.0',
+    version='2.1',
     description='Python password manager',
     long_description=long_description,
     author='Gabriel Bordeaux',
