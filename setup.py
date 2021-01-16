db3195b465cf6da4a11e913a723595bf94d94c65
gab <gabrielbordeaux@gmail.com>
2021-01-16 10:00:06 -0500

    Bump project version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except(IOError, ImportError):
 
 setup(
     name='pyvault',
-    version='2.3.3',
+    version='2.3.4',
     description='Python password manager',
     long_description=long_description,
     author='Gabriel Bordeaux',
