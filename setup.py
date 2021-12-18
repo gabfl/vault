9a6e58edaecfcf9e481214b21b4e93e007b067e5
gab <gabrielbordeaux@gmail.com>
2021-12-17 20:38:12 -0500

    pypandoc.convert -> pypandoc.convert_file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 try:
     import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
+    long_description = pypandoc.convert_file('README.md', 'rst')
 except(IOError, ImportError):
     long_description = open('README.md').read()
 
