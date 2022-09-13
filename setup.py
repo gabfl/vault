d0b2b7d37b80b223b4f2b898e38b8b9e3f5a82d4
Gabriel Bordeaux <gabrielbordeaux@gmail.com>
2022-09-13 17:14:17 -0400

    pep-8 fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 try:
     import pypandoc
     long_description = pypandoc.convert_file('README.md', 'rst')
-except(IOError, ImportError):
+except (IOError, ImportError):
     long_description = open('README.md').read()
 
 setup(
